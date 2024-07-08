7e48600d69403ebafac0716a182edb8f0468a810
brenda lozano <ingblozano651@gmail.com>
2024-07-08 11:07:44 -0500

    primer commit

--- src/pages/perfil.jsx
+++ src/pages/perfil.jsx
@@ -1,14 +1,26 @@
 
 import { Popup } from "../componentes/popUp";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import google from "../assets/img/google.png";
 import apple from "../assets/img/apple.png";
 import esc from "../assets/img/esc.png";
+import firebase from "firebase/compat/app";
+import { auth } from "../configFire/firebase";
+import {createUserWithEmailAndPassword} from "firebase/auth";
 
+export const registro = (user) => {
+    console.log ("hello word");
+    createUserWithEmailAndPassword
+        (auth, user, "")
+        .then(respuesta => {console.log(respuesta) }) 
+}
 
 export const Perfil = () => {
+    
     const [pop, setPop] = useState(false);
 
+    const [user, setUser]= useState("");
+    
     const abrir = () => {
         setPop(true);
     };
@@ -16,14 +28,16 @@ export const Perfil = () => {
         setPop(false);
     };
 
+
     return (
         <div className="relative ">
             {pop && <Popup cerrar={cerrar} />}
             <button className="bg-black text-white p-3 w-full text-start text-xl" onClick={cerrar}><b> Uber</b></button>
             <div className="mt-[3%] flex flex-col  gap-3 text-center w-[201vh] px-[35%]">
                 <h1 className="text-xl text-black"><b>¿Quieres iniciar sesión o Resgistrarte?</b></h1>
-                <input className="border-s-black rounded-lg text-gray-300 p-3" type="text" placeholder=" Introducir número de télefono o email" />
-                <button className="bg-black rounded-lg text-white p-3  hover:bg-gray-500 transition-colors duration-300 ease-in-out">Continuar</button>
+                <input className="border-s-black rounded-lg text-gray-300 p-3" type="text" placeholder=" Introducir número de télefono o email" value={user} onChange={(e) => setUser (e.target.value) }/>
+                
+                <button onClick={registro(user)} className="bg-black rounded-lg text-white p-3  hover:bg-gray-500 transition-colors duration-300 ease-in-out">Continuar</button>
                 <button className="bg-gray-200 font-serif rounded-lg flex gap-[17%] p-3  hover:bg-blue-200 transition-colors duration-300 ease-in-out"><img className="w-[4vh]" src={google} alt="" />Continuar con google</button>
                 <button className="bg-gray-200 font-serif rounded-lg flex gap-[17%] p-3  hover:bg-blue-200 transition-colors duration-300 ease-in-out"><img className="w-[4vh]" src={apple} alt="" />Continuar con Apple</button>
 
